2f547536dad2faf43f2fc6c08cb70787066e22f3
Cristi <cristi.catea@gmail.com>
2014-11-19 17:34:35 -0800

    update

--- js/view.js
+++ js/view.js
@@ -15,11 +15,19 @@ var ContentView = Backbone.View.extend({
 		this.template = options.template;
 	},
 
+	events: {
+		"click a": "submitForm"
+	},
+
 	// Get the template content and render it into a new div-element
 	render: function () {
 		var content = $(this.template).html();
 		$(this.el).html(content);
-
+		console.log($(this.el));
 		return this;
+	},
+
+	submitForm: function (event) {
+		alert("do something here");
 	}
 });
